9fe3501abbb2a0cf160364dc460d55ad9863f5dc
Valeria <vsantanamegia.cf@iesesteveterradas.cat>
2024-10-21 18:42:13 +0200

    pequeños cambios

--- index.php
+++ index.php
@@ -16,33 +16,26 @@
                 <th>Nombre</th>
                 <th>Imagen</th>
             </tr>
-                <?php
-                $profiles = scandir("./profiles", SCANDIR_SORT_ASCENDING);
-                $imageFolder = './images/';
-                
-                foreach ($profiles as $profile) {
-                    if ($profile == "." || $profile == "..") continue; // Ignora los directorios actuales y padres
-                    
-                    $name = pathinfo($profile, PATHINFO_FILENAME); // Obtiene el nombre del archivo sin extensión
-                    $imagePath = $imageFolder . $name . '.jpg';
-                    
-                    // Comprueba si la imagen existe en los diferentes formatos
-                    if (!file_exists($imagePath)) {
-                        $imagePath = $imageFolder . $name . '.jpeg';
-                    }
-                    if (!file_exists($imagePath)) {
-                        $imagePath = $imageFolder . $name . '.png';
-                    }
-                    
-                    // Genera la etiqueta de la imagen o un mensaje de "No disponible"
-                    $imgTag = file_exists($imagePath) ? "<img src='$imagePath' alt='$name'>" : "<div>No disponible</div>";
-                    
-                    echo "<tr>";
-                    echo "<td><a href='profiles/$profile'>$name</a></td>"; // Enlace al perfil
-                    echo "<td>$imgTag</td>"; // Imagen o mensaje
-                    echo "</tr>";
+            <?php
+            $profiles = scandir("./profile", SCANDIR_SORT_ASCENDING);
+            $imageFolder = './img/';
+            foreach ($profiles as $profile) {
+                if ($profile == "." || $profile == "..") continue;
+                $name = pathinfo($profile, PATHINFO_FILENAME);
+                $imagePath = $imageFolder . $name . '.jpg';
+                if (!file_exists($imagePath)) {
+                    $imagePath = $imageFolder . $name . '.jpeg';
                 }
-                ?>
+                if (!file_exists($imagePath)) {
+                    $imagePath = $imageFolder . $name . '.png';
+                }
+                $imgTag = file_exists($imagePath) ? "<img src='$imagePath'>" : "<div>No disponible</div>";
+                echo "<tr>";
+                echo "<td><a href='profile/$profile'>$name</a></td>";
+                echo "<td>$imgTag</td>";
+                echo "</tr>";
+            }
+            ?>
         </table>
     </main>
     <footer>
